356bbb6f6e5aa68e6b8128d0405ffd8cae7b8b03
mhndakbar <makbar@mrsool.co>
2022-02-24 13:14:15 +0300

    Configure mailer and mailhog

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+    def new_user_email
+        user = User.new(name:"Mohanned",email:"mm@mm.com",password:"123456")
+        UserMailer.with(user: user).new_user_email
+    end
 end
